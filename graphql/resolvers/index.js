e0e538d671a242f82206b118a3424731cec9a070
Nathaniel C Napier <nattydevs@gmail.com>
2024-02-04 20:27:17 -0600

    first commit- add all files

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,11 +1,14 @@
 const postsResolvers = require('./posts');
 const usersResolvers = require('./users');
+const commentsResolvers = require('./comments');
 
 module.exports = {
     Query: {
         ...postsResolvers.Query
     },
     Mutation: {
-        ...usersResolvers.Mutation
+        ...usersResolvers.Mutation,
+        ...postsResolvers.Mutation,
+        ...commentsResolvers.Mutation
     }
 };
--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -6,7 +6,23 @@ module.exports = gql`
     body: String!
     createdAt: String!
     username: String!
+    comments: [Comment]!
+    likes: [Like]!
   }
+  type Comment{
+      id: ID!
+      createdAt: String!
+      username: String!
+      body: String!
+  }
+
+  type Like{
+      id: ID!
+      createdAt: String!
+      username: String!
+
+  }
+
   type User {
     id: ID!
     email: String!
@@ -22,9 +38,15 @@ module.exports = gql`
   }
   type Query {
     getPosts: [Post]
+    getPost(postId: ID!): Post
   }
   type Mutation {
     register(registerInput: RegisterInput): User!
     login(username: String!, password: String!): User!
+    createPost(body: String!): Post!
+    deletePost(postId: ID!): String!
+    createComment(postId: String!, body: String!): Post!
+    deleteComment(postId: ID!, CommentId: ID!): Post!
+    likePost(postId: ID!): Post!
   }
 `;

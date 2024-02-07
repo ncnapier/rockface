8f68fd79e4dc7a7c8ec80181ad42436b5a181668
Nathaniel C Napier <nattydevs@gmail.com>
2024-02-06 21:13:30 -0600

    client- react setup- connected to apollo

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -29,7 +29,10 @@ module.exports = {
   Mutation: {
     async createPost(_, { body }, context) {
       const user = checkAuth(context);
-      console.log(user);
+      
+      if (args.body.trim() === '') {
+          throw new Error('Post body must not be empty');
+      }
 
       const newPost = new Post({
         body,

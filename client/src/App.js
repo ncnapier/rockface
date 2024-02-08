22d04a4cfc6056cfb6dac63d69230d60d7eec5b6
Nathaniel C Napier <nattydevs@gmail.com>
2024-02-07 21:24:31 -0600

    semantic ui routes set

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
+// import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
+
+// import 'semantic-ui-css/semantic.min.scc';
 import './App.css';
+
+import MenuBar from './components/MenuBar';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
-    <div>
-      <h1>RockFace</h1>
-    </div>
+    <BrowserRouter>
+      <Container>
+        <MenuBar />
+          <Routes>
+            <Route exact path = '/' Component={Home}/>
+            <Route exact path='/login' Component={Login}/>
+            <Route exact path='/register' Component={Register}/>
+          </Routes>
+          
+          
+      </Container>
+    </BrowserRouter>
   );
 }
 
